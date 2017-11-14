9fb673a6e63d22f3813307d13131856a8cd98469
knudsjef <jeffery.knudson@gmail.com>
2017-11-14 12:33:00 -0600

    Added Parallax scrolling
    Parallax scrolling was added because it got removed for some reason in the new repository :/

---
+++ Trapped Alive Take Two/Assets/Scripts/ParallaxScrolling.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParallaxScrolling : MonoBehaviour {
+
+    [SerializeField]
+    Rigidbody2D playerRigid;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.position = new Vector2(this.transform.position.x + 0.01f * playerRigid.velocity.x, this.transform.position.y + 0.01f * playerRigid.velocity.y);
+	}
+}
